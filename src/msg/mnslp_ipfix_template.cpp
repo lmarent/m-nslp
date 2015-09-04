f2ec0450d3319e8b5110dcfe478b084181548996
Luis Andres Marentes Cubillos <l_marent@yahoo.com>
2015-09-04 15:46:37 -0500

    Update mnslp_ipfix_template.cpp

--- src/msg/mnslp_ipfix_template.cpp
+++ src/msg/mnslp_ipfix_template.cpp
@@ -197,9 +197,28 @@ mnslp_ipfix_template::operator=(const mnslp_ipfix_template &rhs)
 	tsend = rhs.tsend;
 	tid = rhs.tid;
 	maxfields = rhs.maxfields;
-	datafields = rhs.datafields;
-	scopefields = rhs.scopefields;
-	
+
+	templateFieldConstIterList_t it;
+	for( it = rhs.datafields.begin(); it != rhs.datafields.end(); ++it)
+	{
+		ipap_template_field_t ftmp;
+		ftmp.flength = it->flength;
+		ftmp.unknown_f = it->unknown_f;
+		ftmp.relay_f = it->relay_f;
+		ftmp.elem = it->elem;
+		datafields.push_back(ftmp);
+	}	
+
+	for( it = rhs.scopefields.begin(); it != rhs.scopefields.end(); ++it)
+	{
+		ipap_template_field_t ftmp;
+		ftmp.flength = it->flength;
+		ftmp.unknown_f = it->unknown_f;
+		ftmp.relay_f = it->relay_f;
+		ftmp.elem = it->elem;
+		scopefields.push_back(ftmp);
+	}
+
 	return *this;
 }
 
